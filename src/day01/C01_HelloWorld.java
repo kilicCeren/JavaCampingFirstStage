36fd554dab012fdda51ce8df83de1ab34c938ba5
kilicCeren <kiliccrn07@gmail.com>
2024-11-28 12:49:15 +0300

    v1

---
+++ src/day01/C01_HelloWorld.java
@@ -0,0 +1,24 @@
+package day01;
+
+public class C01_HelloWorld {
+
+    public static void main(String[] args) {
+
+        // java'da kullanicinin gorebilecegi mesajlari sout ile olustururuz
+        // Java buradaki yazilari yorum olarak kabul eder
+        // ve bu yazilari kod gibi calistirmaz
+
+
+        System.out.println("Hello World!");
+
+        System.out.println("Java'nin ilk gununden merhaba...");
+
+        /*
+        Bu alanlar
+        arasındaki kısım da
+        yorum paragrafidir
+         */
+
+
+    }
+}
