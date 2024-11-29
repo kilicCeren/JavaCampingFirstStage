29b9eaa8561767f01bfa55eee42374b692150920
kilicCeren <kiliccrn07@gmail.com>
2024-11-29 17:08:58 +0300

    v2

---
+++ src/day02/C02_NonPrimitifDataTurleri.java
@@ -0,0 +1,30 @@
+package day02;
+
+public class C02_NonPrimitifDataTurleri {
+
+    public static void main(String[] args) {
+
+        // non-primitive data turleri bir class'dan olusturuldugu icin
+        // isimleri buyuk harfle baslar
+
+        String metin1 = "Java candir.";
+
+
+        int sayi1 = 10;
+        boolean bl = true;
+        char chr = 'k';
+        double dbl = 34.7;
+
+
+        System.out.println(metin1.toUpperCase()); //  JAVA CANDIR.
+
+        System.out.println(metin1.toLowerCase()); //  java candir.
+
+        System.out.println(metin1.charAt(0)); // J
+        System.out.println(metin1.charAt(2)); // v
+        System.out.println(metin1.charAt(6)); // a
+        System.out.println(metin1.charAt(8)); // d
+
+
+    }
+}
